1b05ebd3613a8558f9f3589ad6aa0c7430048a28
redmasters <erasmo.cjunior@gmail.com>
2021-11-16 13:19:18 -0300

    first commit

---
+++ API/projetoempresa/src/main/java/br/com/redmaster/projetoempresa/ProjetoempresaApplication.java
@@ -0,0 +1,13 @@
+package br.com.redmaster.projetoempresa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoempresaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoempresaApplication.class, args);
+    }
+
+}
---
+++ API/projetoempresa/src/test/java/br/com/redmaster/projetoempresa/ProjetoempresaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.redmaster.projetoempresa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoempresaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
