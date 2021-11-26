3b4383fcdb14378337caa7b47c0b094176d743a1
redmasters <erasmo.cjunior@gmail.com>
2021-11-26 16:42:37 -0300

    Rotas em Angular

--- Angular/helloangular/src/app/app-routing.module.ts
+++ Angular/helloangular/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {CorpoComponent} from "./corpo/corpo.component";
+import {SobreComponent} from "./sobre/sobre.component";
+import {ContatoComponent} from "./contato/contato.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: CorpoComponent},
+  {path: 'sobre', component: SobreComponent},
+  {path: 'contato', component: ContatoComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
