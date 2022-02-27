3fda383477d19b7f378a52d2d869290b6ebe796e
Erasmo Jr <erasmo.junior@gmail.com>
2022-02-27 14:33:00 -0300

    NaturaSSP project added

---
+++ naturassp/src/main/java/io/red/naturassp/NaturaSspApplication.java
@@ -0,0 +1,13 @@
+package io.red.naturassp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaturaSspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaturaSspApplication.class, args);
+	}
+
+}
---
+++ naturassp/src/test/java/io/red/naturassp/NaturaSspApplicationTests.java
@@ -0,0 +1,13 @@
+package io.red.naturassp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaturaSspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
